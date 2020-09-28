93928a5fbfd0a27104ad5cc06873c97988d413d4
Tshweu <t.t.sephiri@gmail.com>
2020-09-29 01:09:14 +0200

    Fixed name

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('./dist/cv-angular8'));
+app.use(express.static('./dist/CV-Angular8'));
 
 app.get("/*",(req,res)=>{
-    res.sendFile('index.html',{root:'dist/cv-angular8'});
+    res.sendFile('index.html',{root:'dist/CV-Angular8'});
 })
 
 app.listen(process.env.PORT || 8080);
