ce7d2c35723b60f0584f26a69cf8db85a5e483ea
Tshweu <t.t.sephiri@gmail.com>
2020-10-07 14:02:45 +0200

    added menu slide in animation

--- src/app/navigation-bar/navigation-bar.component.ts
+++ src/app/navigation-bar/navigation-bar.component.ts
@@ -10,36 +10,33 @@ export class NavigationBarComponent implements OnInit {
   isCollapsed: any = 'true';
   menuClass:any = "";
   menuBar:any="";
+  isMenuCollapsed = true;
 
   constructor() { }
 
   ngOnInit(): void {
     this.menuClass = 'menu-btn';
-    this.menuBar = 'menu-bar';
-  }
-  //
-  // checkCollapse(){
-  //   if(this.isCollapsed)
-  //   {
-  //     this.strClass = 'navbar-menu'+'-'+ 'open';
-  //     this.isCollapsed = false;
-  //   }else{
-  //     this.strClass = 'navbar-menu';
-  //     this.isCollapsed = true;
-  //   }
-  //   // this.isCollapsed = this.isCollapsed === 'navbar-menu' ?  : 'navbar-menu';
-  // }
-
-  doCollapse(){
-    this.menuClass = 'navbar-menu'
+    this.menuBar = 'mb-close';
   }
 
   checkOnClick(){
     if(this.isCollapsed){
-      this.menuClass = 'menu-btn open'
+      this.menuClass = 'menu-btn open';
+      this.menuBar = 'menu-bar';
       this.isCollapsed = false;
     }else{
-      this.menuClass = 'menu-btn'
+      this.menuClass = 'menu-btn';
+      this.menuBar = 'mb-close';
+      this.isCollapsed = true;
+    }
+  }
+
+  checkMenuClick(){
+    if(this.isMenuCollapsed){
+      this.menuBar = 'menu-bar open';
+      this.isMenuCollapsed = false;
+    }else{
+      this.menuBar = 'menu-bar';
       this.isCollapsed = true;
     }
   }
