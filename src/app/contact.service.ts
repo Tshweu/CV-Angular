81ca805b598bf7d8bc5b561a3156b933b078de21
Tshweu <t.t.sephiri@gmail.com>
2020-10-08 12:27:30 +0200

    contact service init

---
+++ src/app/contact.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { from } from 'rxjs';
+import { HttpClientModule } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContactService {
+
+  constructor() { }
+}
