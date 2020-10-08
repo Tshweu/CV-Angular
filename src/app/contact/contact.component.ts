0d9689de12941721f7de5cf5422f32ef827c47a2
Tshweu <t.t.sephiri@gmail.com>
2020-10-08 10:40:51 +0200

    created empty user and added onSubmit method

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ContactUser } from '../contact-user';
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
+  userModel = new ContactUser('','','','','');
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(){
+    console.log(this.userModel);
   }
 
 }
